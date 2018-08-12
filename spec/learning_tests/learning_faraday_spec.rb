23d3cefe0ac775b046bed81124340fa9d1d32170
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-08-12 11:39:04 +0100

    Better comment in learning test

---
+++ spec/learning_tests/learning_faraday_spec.rb
@@ -0,0 +1,12 @@
+require "faraday"
+
+describe "Faraday" do
+    context "Given a third party api that we don't know how to use [OR] has documentation out of date [OR] might introduce breaking changes in the future" do
+        it "describes how to use the functionality we need and the outputs that it produces in the current version" do
+            response = Faraday.get 'https://driftrock-dev-test.herokuapp.com/users'
+            body = JSON.parse(response.body)
+
+            expect(body.keys).to eql(['data'])
+        end
+    end
+end
